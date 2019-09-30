4de50daa33725584ed263ccf58103a30beb41683
José Ramón Méndez Reboredo <moncho.mendez@gmail.com>
2019-10-01 00:07:02 +0200

    Add NLPA configurations to BDREP container

--- bdrep-docker/start.sh
+++ bdrep-docker/start.sh
@@ -2,7 +2,7 @@
 
 mysqld_safe --bind-address=0.0.0.0 &
 sleep 15
-cd /root/bdrep 
-java -jar target/onlinepreprocessor-0.0.1-SNAPSHOT.jar &
 cd /root/bdrep_service
-java -jar target/datasetservice-1.0.0-SNAPSHOT.jar
+java -jar target/datasetservice-1.0.0-SNAPSHOT.jar &
+cd /root/bdrep 
+java -jar target/onlinepreprocessor-0.0.1-SNAPSHOT.jar
