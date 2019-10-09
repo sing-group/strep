206deee2bb28ed40a51cdf19ef98d22d862c77c3
María <mnloures@gmail.com>
2019-10-09 12:30:22 +0200

    Fixed error on  lang change selector

--- src/main/resources/static/js/jqueryFunctions.js
+++ src/main/resources/static/js/jqueryFunctions.js
@@ -1,8 +1,8 @@
 function retrieveDatasetInfo(id) {
-    var url = "/dataset/modal?id="+id;
+    var url = "/dataset/modal?id=" + id;
     console.log(url);
-    $("#dataset-info").load( url, function( response, status, xhr ) {
-        if(response.substr(0, 2)=="<!")
+    $("#dataset-info").load(url, function (response, status, xhr) {
+        if (response.substr(0, 2) == "<!")
         {
             location.reload();
         }
@@ -19,36 +19,36 @@ function updateDatasetsList()
     var dateValue1 = document.getElementById("date1").value;
     var dateValue2 = document.getElementById("date2").value;
 
-    var url = "/dataset/createlist?date1="+dateValue1+"&date2="+dateValue2;
+    var url = "/dataset/createlist?date1=" + dateValue1 + "&date2=" + dateValue2;
 
-    for(var i = 0;i<licenses.length;i++)
+    for (var i = 0; i < licenses.length; i++)
     {
-        if(licenses[i].checked)
+        if (licenses[i].checked)
         {
-            url+=encodeURI("&"+licenses[i].name+"="+licenses[i].value);
+            url += encodeURI("&" + licenses[i].name + "=" + licenses[i].value);
         }
     }
 
-    for(var i = 0;i<languages.length;i++)
+    for (var i = 0; i < languages.length; i++)
     {
-        if(languages[i].checked)
+        if (languages[i].checked)
         {
-            url+=encodeURI("&"+languages[i].name+"="+languages[i].value);
+            url += encodeURI("&" + languages[i].name + "=" + languages[i].value);
         }
     }
 
-    for(var i = 0;i<datatypes.length;i++)
+    for (var i = 0; i < datatypes.length; i++)
     {
-        if(datatypes[i].checked)
+        if (datatypes[i].checked)
         {
-            url+=encodeURI("&"+datatypes[i].name+"="+datatypes[i].value);
+            url += encodeURI("&" + datatypes[i].name + "=" + datatypes[i].value);
         }
     }
 
     console.log(url);
 
-    $("#datasets-list").load( url, function( response, status, xhr ) {
-        if(response.substr(0, 2)=="<!")
+    $("#datasets-list").load(url, function (response, status, xhr) {
+        if (response.substr(0, 2) == "<!")
         {
             location.reload();
         }
@@ -59,75 +59,74 @@ function updateTable(id)
 {
     var datasetNames = document.getElementsByName("datasets");
 
-    if(id=="buttonCheckDatatypes")
+    if (id == "buttonCheckDatatypes")
     {
-    var inputFileNumber = document.getElementById("fileNumber");
-    var datasets = document.getElementsByName("datasets");
+        var inputFileNumber = document.getElementById("fileNumber");
+        var datasets = document.getElementsByName("datasets");
 
-    var inputFileNumberValue = checkNullValues(inputFileNumber);
-    var inputSpamEml = document.getElementById("inputSpam.eml");
-    var inputHamEml = document.getElementById("inputHam.eml");
-    var inputSpamTsms = document.getElementById("inputSpam.tsms");
-    var inputHamTsms = document.getElementById("inputHam.tsms");
-    var inputSpamTytb = document.getElementById("inputSpam.tytb");
-    var inputHamTytb = document.getElementById("inputHam.tytb");
-    var inputSpamTwtid = document.getElementById("inputSpam.twtid");
-    var inputHamTwtid = document.getElementById("inputHam.twtid");
-    var inputSpamWarc = document.getElementById("inputSpam.warc");
-    var inputHamWarc = document.getElementById("inputHam.warc");
+        var inputFileNumberValue = checkNullValues(inputFileNumber);
+        var inputSpamEml = document.getElementById("inputSpam.eml");
+        var inputHamEml = document.getElementById("inputHam.eml");
+        var inputSpamTsms = document.getElementById("inputSpam.tsms");
+        var inputHamTsms = document.getElementById("inputHam.tsms");
+        var inputSpamTytb = document.getElementById("inputSpam.tytb");
+        var inputHamTytb = document.getElementById("inputHam.tytb");
+        var inputSpamTwtid = document.getElementById("inputSpam.twtid");
+        var inputHamTwtid = document.getElementById("inputHam.twtid");
+        var inputSpamWarc = document.getElementById("inputSpam.warc");
+        var inputHamWarc = document.getElementById("inputHam.warc");
 
-    var inputSpamEmlValue = checkNullValues(inputSpamEml);
-    var inputHamEmlValue = checkNullValues(inputHamEml);
-    var inputSpamTsmsValue = checkNullValues(inputSpamTsms);
-    var inputHamTsmsValue = checkNullValues(inputHamTsms);
-    var inputSpamTytbValue = checkNullValues(inputSpamTytb);
-    var inputHamTytbValue = checkNullValues(inputHamTytb);
-    var inputSpamTwtidValue = checkNullValues(inputSpamTwtid);
-    var inputHamTwtidValue = checkNullValues(inputHamTwtid);
-    var inputSpamWarcValue = checkNullValues(inputSpamWarc);
-    var inputHamWarcValue = checkNullValues(inputHamWarc);
+        var inputSpamEmlValue = checkNullValues(inputSpamEml);
+        var inputHamEmlValue = checkNullValues(inputHamEml);
+        var inputSpamTsmsValue = checkNullValues(inputSpamTsms);
+        var inputHamTsmsValue = checkNullValues(inputHamTsms);
+        var inputSpamTytbValue = checkNullValues(inputSpamTytb);
+        var inputHamTytbValue = checkNullValues(inputHamTytb);
+        var inputSpamTwtidValue = checkNullValues(inputSpamTwtid);
+        var inputHamTwtidValue = checkNullValues(inputHamTwtid);
+        var inputSpamWarcValue = checkNullValues(inputSpamWarc);
+        var inputHamWarcValue = checkNullValues(inputHamWarc);
 
-    var url = "/dataset/checkPosibleDatatypes?inputFileNumber="+inputFileNumberValue;
+        var url = "/dataset/checkPosibleDatatypes?inputFileNumber=" + inputFileNumberValue;
 
-    for(var i = 0; i<datasets.length;i++)
-    {
-        if(datasets[i].checked)
+        for (var i = 0; i < datasets.length; i++)
         {
-            url+=encodeURI("&"+datasets[i].name+"="+datasets[i].value);
+            if (datasets[i].checked)
+            {
+                url += encodeURI("&" + datasets[i].name + "=" + datasets[i].value);
+            }
         }
-    }
 
-    url+="&inputSpamEml="+inputSpamEmlValue+"&inputHamEml="+inputHamEmlValue+"&inputSpamTsms="+inputSpamTsmsValue+"&inputHamTsms="+inputHamTsmsValue+
-    "&inputSpamTytb="+inputSpamTytbValue+"&inputHamTytb="+inputHamTytbValue+"&inputSpamTwtid="+inputSpamTwtidValue+"&inputHamTwtid="+inputHamTwtidValue+
-    "&inputSpamWarc="+inputSpamWarcValue+"&inputHamWarc="+inputHamWarcValue;
+        url += "&inputSpamEml=" + inputSpamEmlValue + "&inputHamEml=" + inputHamEmlValue + "&inputSpamTsms=" + inputSpamTsmsValue + "&inputHamTsms=" + inputHamTsmsValue +
+                "&inputSpamTytb=" + inputSpamTytbValue + "&inputHamTytb=" + inputHamTytbValue + "&inputSpamTwtid=" + inputSpamTwtidValue + "&inputHamTwtid=" + inputHamTwtidValue +
+                "&inputSpamWarc=" + inputSpamWarcValue + "&inputHamWarc=" + inputHamWarcValue;
 
-    $("#datatypes-table").load( url, function( response, status, xhr ) {
-          if(response.substr(0, 2)=="<!")
-          {
-              location.reload();
-          }
-      });
-    }
-    else
+        $("#datatypes-table").load(url, function (response, status, xhr) {
+            if (response.substr(0, 2) == "<!")
+            {
+                location.reload();
+            }
+        });
+    } else
     {
-    var url = "/dataset/updateDatatypesTable";
-    for(var i = 0; i<datasetNames.length;i++)
-    {
-        if(datasetNames[i].checked)
+        var url = "/dataset/updateDatatypesTable";
+        for (var i = 0; i < datasetNames.length; i++)
         {
-            if(url=="/dataset/updateDatatypesTable")
-                url+=encodeURI("?"+datasetNames[i].name+"="+datasetNames[i].value);
-            else
-                url+=encodeURI("&"+datasetNames[i].name+"="+datasetNames[i].value);
+            if (datasetNames[i].checked)
+            {
+                if (url == "/dataset/updateDatatypesTable")
+                    url += encodeURI("?" + datasetNames[i].name + "=" + datasetNames[i].value);
+                else
+                    url += encodeURI("&" + datasetNames[i].name + "=" + datasetNames[i].value);
+            }
         }
-    }
-    console.log(url);
-    $("#datatypes-table").load( url, function( response, status, xhr ) {
-        if(response.substr(0, 2)=="<!")
-        {
-            location.reload();
-        }
-    });
+        console.log(url);
+        $("#datatypes-table").load(url, function (response, status, xhr) {
+            if (response.substr(0, 2) == "<!")
+            {
+                location.reload();
+            }
+        });
     }
 }
 
@@ -138,18 +137,18 @@ function checkIfPosibleSpam()
     var datasets = document.getElementsByName("datasets");
 
 
-    var url = "/dataset/checkPosibleSpam?inputSpam="+inputSpam.value+"&fileNumber="+inputFileNumber.value;
+    var url = "/dataset/checkPosibleSpam?inputSpam=" + inputSpam.value + "&fileNumber=" + inputFileNumber.value;
 
-    for(var i = 0; i<datasets.length;i++)
+    for (var i = 0; i < datasets.length; i++)
     {
-        if(datasets[i].checked)
+        if (datasets[i].checked)
         {
-            url+=encodeURI("&"+datasets[i].name+"="+datasets[i].value);
+            url += encodeURI("&" + datasets[i].name + "=" + datasets[i].value);
         }
     }
     console.log(url);
-    $("#info-spam").load( url, function( response, status, xhr ) {
-        if(response.substr(0, 2)=="<!")
+    $("#info-spam").load(url, function (response, status, xhr) {
+        if (response.substr(0, 2) == "<!")
         {
             location.reload();
         }
@@ -158,58 +157,61 @@ function checkIfPosibleSpam()
 
 function checkNullValues(input)
 {
-    if(input!=null)
+    if (input != null)
     {
         return input.value;
-    }
-    else
+    } else
     {
         return 0;
     }
 }
 
-$('#deleteDatasetModal').on('show.bs.modal', function(event)
+$('#deleteDatasetModal').on('show.bs.modal', function (event)
 {
- var button = $(event.relatedTarget);
- var href = button.attr('href');
- var buttonConfirm = $('#deleteConfirmation');
- buttonConfirm.attr("href", href);
+    var button = $(event.relatedTarget);
+    var href = button.attr('href');
+    var buttonConfirm = $('#deleteConfirmation');
+    buttonConfirm.attr("href", href);
 });
 
 $(function () {
     $('[data-toggle="popover"]').popover()
-  })
+})
 
-  function changeLocale(id)
-  {
-      var url = location.href;
-      var index = url.lastIndexOf("?");
+function changeLocale(id)
+{
 
-      if(index==-1)
-      {
-        url+="?lang="+id;
-      }
-      else
-      {
-          index=url.search("lang");
-          if(index==-1)
-          {
-            url+="&lang="+id; 
-          }
-          else
-          {
-            var suburl = url.substr(0,index-1);
+    var url = location.href;
+    var index = url.lastIndexOf("?");
+    if (index == -1)
+    {
+        url += "?lang=" + id;
+    } else
+    {
+        index = url.search("lang");
+        if (index == -1)
+        {
+            url += "&lang=" + id;
+        } else
+        {
+            var suburl = url.substr(0, index - 1);
             index = url.lastIndexOf("?lang");
-            if(index==-1)
+            if (index == -1)
             {
-                url = suburl + "&lang="+id;   
-            }
-            else
+                url = suburl + "&lang=" + id;
+            } else
             {
-                url = suburl + "?lang="+id;   
+                url = suburl + "?lang=" + id;
             }
-          }
-      }
+        }
+    }
+    location.replace(url);
+}
+$(document).ready(function () {
+    let params = new URLSearchParams(location.search);
+    var lang = params.get('lang');
+    if (lang != null) {
+        $('#locales').val(lang);
+    } 
+});
 
-      location.replace(url);
-  }
