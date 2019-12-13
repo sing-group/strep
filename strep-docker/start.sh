94abdeeb4970a68e1004276d537217110ccb5ad2
José Ramón Méndez Reboredo <moncho.mendez@gmail.com>
2019-12-13 12:01:30 +0100

    Fix docker container startup script

--- strep-docker/start.sh
+++ strep-docker/start.sh
@@ -5,12 +5,12 @@ while /bin/true; do
 done &
 sleep 15
 
-cd /root/bdrep_service
+cd /root/strep_service
 while /bin/true; do
     java -jar target/strep.service-1.0.0-SNAPSHOT.jar
 done &
 
-cd /root/bdrep 
+cd /root/strep 
 while /bin/true; do
     java -jar target/strep-0.0.1-SNAPSHOT.jar
 done
